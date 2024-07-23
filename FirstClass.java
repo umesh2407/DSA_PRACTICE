b8a4d5fb4dc731cdc785d1232eb40b64b8672058
umesh2407 <choudharyumesh986@gmail.com>
2024-07-23 21:45:26 +0530

    first program

--- FirstClass.java
+++ FirstClass.java
@@ -1,6 +1,8 @@
 class FirstClass{
     public static void main(String args[]){
-        System.out.println("DSA starts Here!!! ");
-        System.out.println(2+9);
+        // System.out.println("DSA starts Here!!! ");
+        // System.out.println(2+9);
+        char Abc = '@';
+        System.out.println(Abc);
     }
 }
