26e3f23f4c59ed90411c62e2e3084d2de1a94170
umesh2407 <choudharyumesh986@gmail.com>
2024-07-25 21:42:22 +0530

    abstarction

--- Oops.java
+++ Oops.java
@@ -1,28 +1,49 @@
-class Car{
-    String colour;
-    String name;
+// class Car{
+//     String colour;
+//     String name;
 
-    public void Runcar(){
-        System.out.println("Car running .....");
-    }
+//     public void Runcar(){
+//         System.out.println("Car running .....");
+//     }
 
-    public void getcolour(){
-System.out.println(this.colour);
+//     public void getcolour(){
+// System.out.println(this.colour);
+//     }
+// }
+
+// public class Oops {
+//     public static void main(String args[]){
+// Car car1 = new Car();
+// car1.colour = "blue";
+// car1.name = "Fortuner";
+// Car car2 = new Car();
+// car2.colour = "red";
+// car2.name = "Ferrari";
+
+
+// car1.Runcar();
+// car1.getcolour();
+// car2.getcolour();
+//     }
+// }
+
+//Abstaraction
+
+abstract class Animal {
+ abstract void walk();
+}
+
+class Horse extends Animal{
+    public void walk(){
+        System.out.println("Horse walking on 4 legs");
     }
 }
 
-public class Oops {
+public class Oops{
+
     public static void main(String args[]){
-Car car1 = new Car();
-car1.colour = "blue";
-car1.name = "Fortuner";
-Car car2 = new Car();
-car2.colour = "red";
-car2.name = "Ferrari";
+Horse horse = new Horse();
+horse.walk();
 
-
-car1.Runcar();
-car1.getcolour();
-car2.getcolour();
     }
 }
