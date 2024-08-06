c4146d85e6cfca9ed77dc09375b37a5fcd64d9fa
umesh2407 <choudharyumesh986@gmail.com>
2024-08-06 19:47:56 +0530

    added 01 pattern

--- Patterns.java
+++ Patterns.java
@@ -1,16 +1,19 @@
 public class Patterns {
     public static void main(String args[]) {
-        int m = 4;
-        int n = 5;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || j == 1 || i == m || j == n) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
+        int m = 5;
+        int number =1;
+        // int n = 5;
+        for (int i=1; i<=m;i++) { 
+            for (int j = 1; j <=i; j++) {
+                if((i+j)%2 == 0){
+                    System.out.print("1");
                 }
+                else{
+                    System.out.print("0");
+                }
+              
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
