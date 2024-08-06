3b939c5eb374c9b4252c1776fb0e650b863b5864
umesh2407 <choudharyumesh986@gmail.com>
2024-08-06 18:32:02 +0530

    added sum program

--- Loops.java
+++ Loops.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 public class Loops{
     public static void main(String args[]){
-        for(int i =0; i<20; i++){
-            System.out.println("Umesh");
+        // for(int i =0; i<=10; i++){
+        //     System.out.print(i+" ");
+        // }
+        // int i=0;
+        // while(i<=10){
+        //     System.out.println(i);
+        //     i++;
+        // }
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+        int sum=0;
+        for(int i=0; i<=n; i++){
+            sum = sum + i;
         }
+        System.out.println("Sum is:"+sum);
     }
 }
