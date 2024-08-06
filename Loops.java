1e375f5734654f3f1344e3dbedd06aa7c2c9df72
umesh2407 <choudharyumesh986@gmail.com>
2024-08-06 18:51:00 +0530

    added patern basics

--- Loops.java
+++ Loops.java
@@ -12,7 +12,7 @@ public class Loops{
         // }
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-        for(int i=0; i<=10; i++){
+        for(int i=1; i<=10; i++){
             System.out.println(n+" x "+i+" = "+(i*n));
         }
     }
---
+++ Patterns.java
@@ -0,0 +1,12 @@
+public class Patterns{
+    public static void main(String args[]){
+        int m=8;
+        int n=10;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
