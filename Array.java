842bb0bd2fa3df1dd18d121345a5829c4399d845
umesh2407 <choudharyumesh986@gmail.com>
2024-08-07 21:17:35 +0530

    addded arrays

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String args[]){
+        int rollno[] = new int[4];
+        rollno[0]= 13;
+        rollno[1]=15;
+        rollno[2]=18;
+        rollno[3]=19;
+for(int i=0; i<4; i++){
+    System.out.println(rollno[i]);
+
+}
+    }
+}
