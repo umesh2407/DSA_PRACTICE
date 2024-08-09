89ae121f2d23767e4a23b3fe968c152730e0baf1
umesh2407 <choudharyumesh986@gmail.com>
2024-08-09 20:50:10 +0530

    added strings

--- Strings.java
+++ Strings.java
@@ -5,7 +5,7 @@ public class Strings{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        System.out.println("Your name is : "+name);
+        System.out.println("Your name length is : "+name.length());
 
     }
 }
