d336797baa33023579e404611568e99d1d567c40
umesh2407 <choudharyumesh986@gmail.com>
2024-08-11 13:02:40 +0530

    added append code

--- StringBuilders.java
+++ StringBuilders.java
@@ -2,7 +2,14 @@ public class StringBuilders {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("TONY");
         System.out.println(sb.charAt(1));
-        sb.setCharAt(0, 'P');
+        // sb.setCharAt(0, 'P');
+        // sb.delete(3,4);
+        sb.append("S");
+        sb.append("T");
+        sb.append("A");
+        sb.append("R");
+        sb.append("K");
+
         System.out.println(sb);
     }
 }
