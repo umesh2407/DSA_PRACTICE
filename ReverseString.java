9385660f9f1a9087075daae381d4765317c6cc94
umesh2407 <choudharyumesh986@gmail.com>
2024-08-11 14:22:38 +0530

    reverse string

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public static void main(String[] args) {
+       
+        StringBuilder sb = new StringBuilder("Umesh");
+      for(int i=0; i< sb.length()/2; i++){
+
+        int front = i;
+        int back = sb.length() - 1 - i;
+        char frontchar = sb.charAt(front);
+        char backchar = sb.charAt(back);
+
+        sb.setCharAt(front, backchar);
+        sb.setCharAt(back, frontchar);
+
+      }
+System.out.println(sb);
+    }
+}
