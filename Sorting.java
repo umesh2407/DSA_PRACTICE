b19efb72af6db7f6fd23c20cead2e1d49b565db1
umesh2407 <choudharyumesh986@gmail.com>
2024-08-13 20:31:10 +0530

    selection sort

--- Sorting.java
+++ Sorting.java
@@ -10,15 +10,28 @@ public class Sorting {
         int arr[] = { 2, 4, 7, 5, 3 };
         int n = arr.length;
         // Bubble sort
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swapping
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        // for (int i = 0; i < n - 1; i++) {
+        //     for (int j = 0; j < n - i - 1; j++) {
+        //         if (arr[j] > arr[j + 1]) {
+        //             // Swapping
+        //             int temp = arr[j];
+        //             arr[j] = arr[j + 1];
+        //             arr[j + 1] = temp;
+        //         }
+        //     }
+        // }
+        //Selection Sort
+        for(int i = 0; i<n-1; i++){
+           int smallest = i;
+           for(int j=i+1; j<n; j++){
+                if(arr[smallest]> arr[j]){
+                    smallest = j;
                 }
-            }
+           }
+int temp = arr[smallest];
+arr[smallest]=arr[i];
+arr[i] = temp;
+
         }
         printArray(arr);
     }
