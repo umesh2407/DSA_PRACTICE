3946049af6f4970b0a9e8e5fd539fa7b7bfafb44
umesh2407 <choudharyumesh986@gmail.com>
2024-08-13 18:53:39 +0530

    added

---
+++ Sorting.java
@@ -0,0 +1,25 @@
+public class Sorting {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 7, 5, 3 };
+        int n = arr.length;
+        // Bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swapping
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
