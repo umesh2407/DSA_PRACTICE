d5e3007328067d7dfa1f502246357da100bd73c1
umesh2407 <choudharyumesh986@gmail.com>
2024-08-22 21:33:00 +0530

    added code

--- Recursions.java
+++ Recursions.java
@@ -1,3 +1,5 @@
+//Recursions programs
+
 // public class Recursions {
  
 //     public static void printSumOfNum(int i, int n, int sum) {
@@ -16,20 +18,36 @@
 // }
 
 //Factorial program
-import java.util.*;
-public class Recursions{
+// import java.util.*;
+// public class Recursions{
 
-    public static int Factorial(int n){
-if(n == 1 || n==0){
-   return 1;
-}
-  int ans = n * Factorial(n-1);
-return ans;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-       int a =  sc.nextInt();
-        int fact = Factorial(a);
-        System.out.println(fact);
-    }
-}
+//     public static int Factorial(int n){
+// if(n == 1 || n==0){
+//    return 1;
+// }else{
+// return(n*Factorial(n-1));
+// }
+// //   int ans = n * Factorial(n-1);
+// // return ans;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//        int a =  sc.nextInt();
+//         int fact = Factorial(a);
+//         System.out.println(fact);
+//     }
+// }
+
+// //fibonacci series 
+// public class Recursions {
+
+//     public static void fibonacci(int a, int b, int n){
+//         int c = a + b;
+// System.out.print(c);
+// fibonacci(b,c,n-1);
+//     }
+//     public static void main(String[] args) {
+//         int n = 5;
+//         fibonacci(0,1,5);
+//     }
+// } 
