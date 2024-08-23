810837741047563f0f0da04d030c13f8fb4f5930
umesh2407 <choudharyumesh986@gmail.com>
2024-08-23 14:20:00 +0530

    swapping without variable

---
+++ Swapping.java
@@ -0,0 +1,12 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        a = a+b;
+        b= a-b;
+        a= a-b;
+
+        System.out.println("a is  " + a+"  and  "+"b is  "+b);
+
+    }
+}
